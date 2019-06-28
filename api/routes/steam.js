2bffd564d6594c70d67d446f476bbb7a8af43e23
sai mun <saimun92@gmail.com>
2019-06-28 10:22:38 +0800

    backend done

--- api/routes/steam.js
+++ api/routes/steam.js
@@ -1,45 +1,51 @@
-// I've cracked the code!!!!!!!
+// https://github.com/xDimGG/node-steamapi
 var request = require('request');
 var express = require('express');
 var router = express.Router();
 var SteamAPI = require('steamapi');
 var steam = new SteamAPI('0693EFD0257BE02F03DF6CF8BC57986D');
 
-router.get('/', function(req, res, next) {
-    steam.resolve('https://steamcommunity.com/id/icecoolcat').then(id => {
+// Get the User
+router.get('/user-summary/:name', function(req, res, next) {
+    steam.resolve(`https://steamcommunity.com/id/${req.params.name}`).then(id => {
         steam.getUserSummary(id).then(summary => {
-            console.log(summary);
             res.send(summary);
-            /**
-            PlayerSummary {
-                avatar: {
-                    small: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/7f/7fdf55394eb5765ef6f7be3b1d9f834fa9c824e8.jpg',
-                    medium: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/7f/7fdf55394eb5765ef6f7be3b1d9f834fa9c824e8_medium.jpg',
-                    large: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/7f/7fdf55394eb5765ef6f7be3b1d9f834fa9c824e8_full.jpg'
-                },
-                steamID: '76561198146931523',
-                url: 'http://steamcommunity.com/id/DimGG/',
-                created: 1406393110,
-                lastLogOff: 1517725233,
-                nickname: 'Dim',
-                primaryGroupID: '103582791457347196',
-                personaState: 1,
-                personaStateFlags: 0,
-                commentPermission: 1,
-                visibilityState: 3
-            }
-            */
         });
+    }).catch(err => {
+        console.log(err);
+        res.send("oops no such user!");
     });
 });
 
-router.get('/recent', function(req, res, next) {
-    steam.resolve('https://steamcommunity.com/id/icecoolcat').then(id => {
-        steam.getUserRecentGames(id).then(summary => {
-            console.log(summary);
-            res.send(summary);
-        });
+router.get('/recent/:id', function(req, res, next) {
+    steam.getUserRecentGames(`${req.params.id}`).then(summary => {
+        res.send(summary);
     });
 });
+
+router.get('/ownedgames/:id', function(req, res, next) {
+    steam.getUserOwnedGames(`${req.params.id}`).then(summary => {
+        res.send(summary);
+    });
+});
+
+router.get('/getfriends/:id', function(req, res, next) {
+    steam.getUserFriends(`${req.params.id}`).then(summary => {
+        res.send(summary);
+    });
+});
+
+router.get('/getachievements/:id', function(req, res, next) {
+    steam.getUserAchievements(`${req.params.id}`).then(summary => {
+        res.send(summary);
+    });
+});
+
+router.get('/getbadges/:id', function(req, res, next) {
+    steam.getUserBadges(`${req.params.id}`).then(summary => {
+        res.send(summary);
+    });
+});
+
 
 module.exports = router;
