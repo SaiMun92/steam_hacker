091d0e7d30795ad4cbcc90f9e56d4eef993742d6
sai mun <saimun92@gmail.com>
2019-07-06 19:20:14 +0800

    make connecton to api

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/styles';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
+import axios from 'axios';
 import './App.css';
 
-const useStyles = makeStyles({
+const useStyles = {
   root: {
     padding: '2px 4px',
     display: 'flex',
@@ -25,29 +27,60 @@ const useStyles = makeStyles({
     height: 28,
     margin: 4,
   },
-});
+};
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    };
 
-function App() {
-  const classes = useStyles();
+    this.execute = this.execute.bind(this);
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* Search Bar */}
-        <Paper className={classes.root}>
-          <InputBase
-            className={classes.input}
-            placeholder="Enter Steam IGN"
-            inputProps={{ 'aria-label': 'Enter Steam IGN' }}
-          />
-          <IconButton className={classes.iconBuWtton} aria-label="Search">
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-      </header>
-    </div>
-  );
+  async execute() {
+    const userdata = await axios.get(`http://localhost:9000/steam//user-summary/${this.state.input}`);
+    console.log(userdata);
+
+    // do something about the returned data;
+  }
+
+  enterPressed(event) {
+    let code = event.keyCode || event.which;
+
+    if (code === 13) {
+      this.execute();
+    }
+  }
+ 
+  render() {
+    const { classes } = this.props;
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* Search Bar */}
+          <Paper className={classes.root}>
+            <InputBase
+              className={classes.input}
+              placeholder="Enter Steam IGN"
+              inputProps={{ 'aria-label': 'Enter Steam IGN' }}
+              onChange={(event) => this.setState({ input: event.target.value })}
+              onKeyPress={this.enterPressed.bind(this)}
+            />
+            <IconButton className={classes.iconBuWtton} aria-label="Search" onClick={this.execute}>
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(useStyles)(App);
